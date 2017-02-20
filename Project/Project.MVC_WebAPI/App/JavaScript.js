770a1d859bdd0cd466e4f03885783fbab9587dd6
Petar Bagaric <pbagaric1@gmail.com>
2017-02-20 12:39:37 +0100

    Zapocet angular, napravljen GET

---
+++ Project/Project.MVC_WebAPI/App/JavaScript.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="D:\praksa-level3\Project\Project.MVC_WebAPI\Scripts/angular.min.js" />
+var app = angular.module('app', []);
+
+app.controller("myController", function ($scope, $http) {
+    $scope.vehicles = [];
+            $http.get('http://localhost:50696/api/VehicleMake/GetAll')
+                .then(function (response) {
+                    $scope.vehicles = response.data;
+                });
+        });
